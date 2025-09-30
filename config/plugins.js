e680618d3dab6d4603414f7e6ea0df3700a10a0a
Romain Wirth <romain.wirth@gmail.com>
2025-09-30 17:51:29 +0200

    feature autoPopulateText

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,10 @@
-module.exports = () => ({});
+module.exports = {
+  upload: {
+    config: {
+      sizeOptimization: true,
+      responsiveDimensions: true,
+      // Auto-populate alt text avec le nom de fichier
+      autoPopulateAltText: true,
+    },
+  },
+};
